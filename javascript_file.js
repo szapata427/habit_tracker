c1708bc7712265e954ae0ee10f6580a846e6fb8c
steven <szapata427@yahoo.com>
2019-04-14 19:47:03 -0400

    added input values to be shown

--- javascript_file.js
+++ javascript_file.js
@@ -1,18 +1,23 @@
-window.onload=function(){
+$(document).ready(function () {
 
-var newHabitName = document.getElementById("input-habitname")
 
-arrayHabits = []
+// var newHabitName = document.getElementById("input-habitname")
+// var submitNewHabit = document.getElementById("habit-submit")
+// var submitNewHabit = document.getElementsByClassName("habit-form-submit-button")[0]
 
-// function addNewHabitName() {
-//     if (newHabitName) {
-        
-//     }
-    
-// }
 
-newHabitName.addEventListener('keyup', function(event) {
-    
-    })
+$("form").submit(function(event){
+    event.preventDefault()
+    // alert("Submitted");
+    var namevalue = $("input[name=habitname]").val()
+    var daysvalue = $("input[name=days]").val()
+    var progressvalue = $("input[name=progress]").val()
+    var reasonvalue = $("input[name=reason]").val()
+    // var inputvalue = $("#input-habitname").val()
+    console.log(event)
+    console.log(namevalue)
+  });
 
-}
+
+
+});
