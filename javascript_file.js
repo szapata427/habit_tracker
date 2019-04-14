cac27a4da0e419d0fa062abf2d684b0927a02d64
steven <szapata427@yahoo.com>
2019-04-13 23:18:20 -0400

    first commit on this project

---
+++ javascript_file.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+
+var newHabitName = document.getElementById("input-habitname")
+
+arrayHabits = []
+
+// function addNewHabitName() {
+//     if (newHabitName) {
+        
+//     }
+    
+// }
+
+newHabitName.addEventListener('keyup', function(event) {
+    
+    })
+
+}
